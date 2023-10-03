9957aeed3fa5a24e6bcda755f50160b401e07c23
nayemhasan45 <nayemhasan75907@gmail.com>
2023-10-03 19:24:04 +0100

    first conditional program

---
+++ w0m3/condition.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    if(x<=5){
+
+        printf("we can go home");
+
+    }else
+    {
+        printf("value is greater than 5");
+    }
+    
+
+    return 0;
+}
