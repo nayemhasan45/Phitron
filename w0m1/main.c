5b785b133c136afabf1c81d7701eed425960976a
nayemhasan45 <nayemhasan75907@gmail.com>
2023-10-03 18:45:49 +0100

    add initial file

---
+++ w0m1/main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int rahim = 123, tk=321;
+    float karim = 23.4;
+    // printf("hello motherfucker 2");
+    // printf("%d %d %f",rahim,karim,tk);
+    char nana='a';
+    // printf("%c",nana);
+    int kaka,mama;
+    scanf("%d %d",&kaka,&mama);
+    printf("%d %d",kaka,mama);
+
+    return 0;
+}
