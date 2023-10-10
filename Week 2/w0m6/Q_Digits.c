4686cb85f17ccd4b50c1a159185bb2cd48f64d16
nayemhasan45 <nayemhasan75907@gmail.com>
2023-10-10 19:07:02 +0100

    nested loop problem solved

---
+++ Week 2/w0m6/Q_Digits.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        int x;
+        scanf("%d",&x);
+        do
+        {
+            printf("%d ",x%10);
+            x=x/10;
+        } while (x!=0);
+        printf("\n");
+    }
+    return 0;
+}
