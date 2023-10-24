bb30e04ac3336103551d9dadfcceff7151585878
nayemhasan45 <nayemhasan75907@gmail.com>
2023-10-24 20:42:23 +0100

    third problem solved of codeforces

---
+++ Week 2/w0m7.5(practice day)/C_Replacement.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i]>0)
+        {
+            arr[i]=1;
+        }else if (arr[i]<0)
+        {
+            arr[i]=2;
+        }
+        printf("%d ",arr[i]);
+        
+        
+    }
+    return 0;
+}
