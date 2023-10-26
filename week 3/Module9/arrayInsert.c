63547978ecf6b57144c77ffba684a5b14e7619fa
nayemhasan45 <nayemhasan75907@gmail.com>
2023-10-26 23:10:09 +0100

    array insert try for the first time

---
+++ week 3/Module9/arrayInsert.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n+1];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int pos,value;
+    scanf("%d %d",&pos,&value);
+    for (int i = n; i > pos; i--)
+    {
+        arr[i]=arr[i-1];
+    }
+    arr[pos]=value;
+    for (int i = 0; i <= n; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    
+    return 0;
+}
