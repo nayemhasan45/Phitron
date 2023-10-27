7c1b6a9f4f9ace62346806a3aa35d179d7027c0f
nayemhasan45 <nayemhasan75907@gmail.com>
2023-10-27 14:43:46 +0100

    string initilize learned

---
+++ week 3/Module10/string.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char ar[6]="nayem";
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     scanf("%c",&ar[i]);
+    // }
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     printf("%c",ar[i]);
+    // }
+    // scanf("%s",&ar);
+    printf("%s",ar);
+    
+    return 0;
+}
