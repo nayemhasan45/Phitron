296a2595fde8c56a08f043712ded6dfeb7b09e8e
nayemhasan45 <nayemhasan75907@gmail.com>
2023-11-04 09:18:14 +0000

    pattern3 done

---
+++ week 4/Module13/practiceP2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int k=n;
+    for (int i = 0; i < n; i++)
+    {
+        for(int j=1;j<=k;j++){
+            printf("%d ",j);
+        }
+        k--;
+        printf("\n");
+    }
+    
+    return 0;
+}
