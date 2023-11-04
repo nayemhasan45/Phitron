d3f9b7c66c9ab1aca964e46d5788bc290e355cb9
nayemhasan45 <nayemhasan75907@gmail.com>
2023-11-04 09:42:39 +0000

    third task done

---
+++ week 4/Module13/task3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,s,k;
+    scanf("%d",&n);
+    s=n-1;
+    k=1;
+    for (int i = 0; i < n; i++)
+    {
+        for(int j=1;j<=s;j++){
+            printf(" ");
+        }
+        for(int j=1;j<=k;j++){
+            printf("*");
+        }
+        s--;
+        k++;
+        printf("\n");
+    }
+    
+    return 0;
+}
