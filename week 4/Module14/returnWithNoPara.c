a570d72718c66a9e369029d6ce681a56cf9db3f7
nayemhasan45 <nayemhasan75907@gmail.com>
2023-11-04 12:35:34 +0000

    return with no parameter

--- week 4/Module14/returnWithNoPara.c
+++ week 4/Module14/returnWithNoPara.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 //without using void
-// int sum(){
-//     int a,b;
-//     scanf("%d %d",&a,&b);
-//     int s=a+b;
-//     return s;
-// }
+int sum(){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    int s=a+b;
+    return s;
+}
+// using void as parameter
 int sum(void){
     int a,b;
     scanf("%d %d",&a,&b);
@@ -14,6 +15,7 @@ int sum(void){
 }
 int main()
 {
+    //if you use parameter when it's written void it will give error.
     int s = sum(200);
     printf("%d",s);
     return 0;
