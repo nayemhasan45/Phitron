a44e844166bcd97baa1ad4a0afb4785f288a8f41
nayemhasan45 <nayemhasan75907@gmail.com>
2023-11-04 11:42:08 +0000

    some of a two num number is of an array

---
+++ week 4/Module13/sumOfValueEx.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int x;
+    scanf("%d",&x);
+    int flag =0;
+    for (int i = 0; i < n-1; i++)
+    {
+        for(int j=i+1;j<n;j++){
+            if (arr[i]+arr[j]==x)
+            {
+                flag=1;
+            }
+            
+        }
+    }
+    if (flag)
+    {
+        printf("YES");
+    }else{
+        printf("NO");
+    }
+    
+    
+    
+    return 0;
+}
