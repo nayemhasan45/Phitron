4430e70df5a68469108c08db56b0db3ad7c9c2b9
nayemhasan45 <nayemhasan75907@gmail.com>
2023-11-16 19:49:03 +0000

    c++ start

---
+++ C++/week 1/module 1/ternary.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main (){
+
+    int x;
+    cin>>x;
+    (x%2==0)? cout<<"even"<<endl:cout<<"odd"<<endl;
+    return 0 ;
+}
---
+++ C++/week 1/module 1/test.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main (){
+    cout<<"hello world";
+    float x ;
+    cin>>x;
+    cout<<fixed<<setprecision(3)<<x<<endl;
+    return 0;
+}
