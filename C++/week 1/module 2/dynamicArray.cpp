e3b3d552b869c3385f0feb892a31007e8a0f2a67
nayemhasan45 <nayemhasan75907@gmail.com>
2023-11-17 19:09:52 +0000

    dynamic arry

---
+++ C++/week 1/module 2/dynamicArray.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    int n = 5;
+    int *a = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>a[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout<<a[i];
+    }
+    
+    return 0;
+}
