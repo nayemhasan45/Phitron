ff0c8ce139938d10805a62e592e21fc3dadc04d7
nayemhasan45 <nayemhasan75907@gmail.com>
2023-11-17 21:28:34 +0000

    second problem solved

---
+++ C++/week 1/module 2.5p/C_Simple_Calculator.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+
+    long long int n,m;
+    cin>>n>>m;
+    cout<<n<<" "<<"+"<<" "<<m<<" "<<"="<<" "<<n+m<<endl;
+    cout<<n<<" "<<"*"<<" "<<m<<" "<<"="<<" "<<n*m<<endl;
+    cout<<n<<" "<<"-"<<" "<<m<<" "<<"="<<" "<<n-m;
+    return 0;
+}
