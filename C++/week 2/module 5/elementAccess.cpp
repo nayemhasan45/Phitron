771864520b629f74a915b1d01e0d6c4f487c6c56
nayemhasan45 <nayemhasan75907@gmail.com>
2023-11-21 10:50:35 +0000

    element access learned of string

---
+++ C++/week 2/module 5/elementAccess.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+
+    string s;
+    cin>>s;
+    cout<<s.front()<<endl;
+    cout<<s.back()<<endl;
+    cout<<s.size();
+    return 0;
+}
