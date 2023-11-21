53091d0d17cde1e22e0ada915ce332af3dd3544a
nayemhasan45 <nayemhasan75907@gmail.com>
2023-11-21 10:44:18 +0000

    string class learned and practice capacity function

---
+++ C++/week 2/module 5/string.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+
+    string s;
+    s="hello world";
+    // s="hello";
+    // cout<<s.max_size()<<"\n"; //for max size of an stirng
+    cout<<s<<endl;
+    cout<<s.size()<<"\n";
+    string s2;
+    // cout<<s2.capacity()<<endl;
+    // s.clear();
+    if (s.empty())
+    {
+        cout<<"empty"<<endl;
+    }else{
+        cout<<"not empty"<<endl;
+    }
+    s.resize(5);
+    s.resize(11,'n');
+    cout<<s<<endl;
+    return 0;
+}
