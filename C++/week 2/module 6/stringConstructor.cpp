aef77723e7acc27a396d528efafc6835bea34921
nayemhasan45 <nayemhasan75907@gmail.com>
2023-11-22 21:55:08 +0000

    string constructor learned

---
+++ C++/week 2/module 6/stringConstructor.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    // string s="hello world";
+    //1
+    // string s("hello world");
+    //2
+    // string s("hello world",4);
+    //3
+    // string a="hello world";
+    // string s(a,4);
+    //4
+    string s(3,'x');
+    cout<<s<<endl;
+    return 0;
+}
