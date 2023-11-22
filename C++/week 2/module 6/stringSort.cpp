e41c2ac9d942cc944462b32268dd9e5d266ba3cd
nayemhasan45 <nayemhasan75907@gmail.com>
2023-11-22 22:03:04 +0000

    sort with string

---
+++ C++/week 2/module 6/stringSort.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+
+    string s;
+    // cin>>s;
+    getline(cin,s);
+    sort(s.begin(),s.end());
+    // getchar();
+    cout<<s<<endl;
+    getline(cin,s);
+    sort(s.begin(),s.end());
+    cout<<s;
+    return 0;
+}
