fea3661f61c8e7d6a0cc3afe7749e129da0d5fb2
nayemhasan45 <nayemhasan75907@gmail.com>
2023-11-22 22:07:57 +0000

    ranged based for loop learned

---
+++ C++/week 2/module 6/rengedBasedForLoop.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+
+    string s;
+    cin>>s;
+    // for (int i = 0; i < s.size(); i++)
+    // {
+    //     cout<<s[i]<<endl;
+    // }
+    for(char n:s){
+        cout<<n<<endl;
+    }
+    
+
+    return 0;
+}
