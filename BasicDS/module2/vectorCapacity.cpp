f7c09f774a5bb001b045c56d65f3d285b4195f80
nayemhasan45 <nayemhasan75907@gmail.com>
2023-11-29 20:02:41 +0000

    vector cpacity learned

---
+++ BasicDS/module2/vectorCapacity.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    // vector<int> v(5,2*2);
+    vector<int> v={1,2,3,4,5};
+    cout<<v.size()<<endl;
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    
+    return 0;
+}
