5c1b698002e24102854497d64387138936e40933
nayemhasan45 <nayemhasan75907@gmail.com>
2023-12-21 11:07:09 +0000

    reverse dubly liinklist

---
+++ BasicDS/week 3_doubly_link_list/module 10_STL_list_and_cycle_detection/reverse_dubly_link_list.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+        int val;
+        Node*next;
+        Node*prev;
+        Node(int val){
+                this->val=val;
+                this->next=NULL;
+                this->prev=NULL;
+            }
+};
+void printNode(Node*head){
+    Node*tem=head;
+    while (tem!=NULL)
+    {
+        cout<<tem->val<<" ";
+        tem=tem->next;
+    }
+    cout<<endl;
+}
+void reverseList(Node*head,Node*tail){
+    Node*i=head;
+    Node*j=tail;
+    while (i!=j&&i->next!=j)
+    {
+        swap(i->val,j->val);
+        i=i->next;
+        j=j->prev;
+    }
+}
+int main (){
+    Node*head=new Node(10);
+    Node*a=new Node(20);
+    Node*b=new Node(30);
+    Node*c=new Node(40);
+    Node*tail=c;
+    head->next=a;
+    a->prev=head;
+    a->next=b;
+    b->prev=a;
+    b->next=c;
+    c->prev=b;
+    reverseList(head,tail);
+    printNode(head);
+    return 0;
+}
