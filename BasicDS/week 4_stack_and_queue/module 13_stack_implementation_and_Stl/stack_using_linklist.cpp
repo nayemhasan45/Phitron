54a57f3200087c23d53facf23c4dcd898e979848
nayemhasan45 <nayemhasan75907@gmail.com>
2024-01-14 20:46:39 +0000

    stack using linkedlist

---
+++ BasicDS/week 4_stack_and_queue/module 13_stack_implementation_and_Stl/stack_using_linklist.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+        int val;
+        Node*next;
+        Node*prev;
+        Node(int val){
+                this->val=val;
+                this->next=NULL;
+                this->prev=NULL;
+            }
+};
+class myStack{
+public:
+    Node*head=NULL;
+    Node*tail=NULL;
+    int sz=0;
+    void push(int val){
+        Node*newNode=new Node(val);
+        sz++;
+        if(head==NULL){
+            head=newNode;
+            tail=newNode;
+        }
+        newNode->prev=tail;
+        tail->next=newNode;
+        tail=tail->next;
+    }
+    void pop(){
+        sz--;
+        Node*deleteNode=tail;
+        tail=tail->prev;
+        if(tail==NULL){
+            head=NULL;
+        }else{
+            tail->next=NULL;
+        }
+        delete deleteNode;
+    }
+    int size(){
+        return sz;
+    }
+    int top(){
+        return tail->val;
+    }
+    bool empty(){
+        if(sz==0) return true;
+        else return false;
+    }
+};
+int main (){
+    myStack st;
+    int n;
+    cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin>>x;
+        st.push(x);
+    }
+    while (!st.empty())
+    {
+        cout<<st.top()<<endl;
+        st.pop();
+    }
+    return 0;
+}
