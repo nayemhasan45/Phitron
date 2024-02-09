e768a31260213227a0741fc27a0f163fa904ffdb
nayemhasan45 <nayemhasan75907@gmail.com>
2024-02-09 00:26:22 +0000

    inorder traversal learned

---
+++ BasicDS/week 5_Binary_tree/m_17_binary_tree_implementation/inorder_traversal.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+        int val;
+        Node*right;
+        Node*left;
+        Node(int val){
+            this->val=val;
+            this->right=NULL;
+            this->left=NULL;
+        }
+};
+void inorderPrint(Node*root){
+    if(root==NULL){
+        return;
+    }
+    inorderPrint(root->left);
+    cout<<root->val<<" ";
+    inorderPrint(root->right);
+}
+int main (){
+    Node*root=new Node(10);
+    Node*a=new Node(20);
+    Node*b=new Node(30);
+    Node*c=new Node(40);
+    Node*d=new Node(50);
+    Node*e=new Node(60);
+    Node*f=new Node(70);
+    Node*g=new Node(80);
+    Node*h=new Node(90);
+    Node*i=new Node(100);
+    //connection 
+    root->left=a;
+    root->right=b;
+    a->left=c;
+    a->right=h;
+    c->right=e;
+    b->right=d;
+    d->right=g;
+    d->left=f;
+    h->right=i;
+    //call
+    inorderPrint(root);
+    return 0;
+}
