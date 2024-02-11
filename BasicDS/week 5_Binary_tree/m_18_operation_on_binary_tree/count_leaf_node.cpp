849d520d6de0cf2ee1f3a2bdb463735dd240bfe0
nayemhasan45 <nayemhasan75907@gmail.com>
2024-02-11 16:46:09 +0000

    learned how to count leaf node

---
+++ BasicDS/week 5_Binary_tree/m_18_operation_on_binary_tree/count_leaf_node.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+        int val;
+        Node*right;
+        Node*left;
+        Node(int val){
+            this->val=val;
+            this->right=NULL;
+            this->left=NULL;
+        }
+};
+Node*binaryInput(){
+    int val;
+    cin>>val;
+    Node*root;
+    if(val==-1) root=NULL;
+    else root=new Node(val);
+    queue<Node*>q;
+    if(root)
+        q.push(root);
+    while (!q.empty())
+    {
+        Node*p=q.front();
+        q.pop();
+        int l,r;
+        cin>>l>>r;
+        Node*left,*right;
+        if(l==-1)
+            left=NULL;
+        else
+            left=new Node(l);
+        if(r==-1)
+            right=NULL;
+        else 
+            right=new Node(r);
+        p->left=left;
+        p->right=right;
+        if(p->left)
+            q.push(p->left);
+        if(p->right)
+            q.push(p->right);
+    }
+    return root;
+}
+void printBNode(Node*root){
+    queue<Node*>q;
+    if(root) q.push(root);
+    else {
+        cout<<"tree is empty";
+        return;
+    }
+    while (!q.empty())
+    {
+        Node*f=q.front();
+        q.pop();
+        cout<<f->val<<" ";
+        if(f->left) q.push(f->left);
+        if(f->right) q.push(f->right);
+    }
+}
+int cntLeafNode(Node*root){
+    if(root==NULL) return 0;
+    if(root->left==NULL&&root->right==NULL) return 1;
+    else{
+        int l=cntLeafNode(root->left);
+        int r=cntLeafNode(root->right);
+        return l+r;
+    }
+}
+int main (){
+    Node*root=binaryInput();
+    // printBNode(root);
+    cout<<cntLeafNode(root);
+    return 0;
+}
